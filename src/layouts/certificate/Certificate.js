6539a0def325bb2c979e9582e307650b12cc2157
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2019-11-15 04:05:18 +0800

    Refactor code

--- src/layouts/certificate/Certificate.js
+++ src/layouts/certificate/Certificate.js
@@ -3,36 +3,35 @@ import styled from 'styled-components';
 import background from '../../static/certificate.jpg';
 import { spacing, palette } from '@material-ui/system';
 
-
 function Main(props){
 
 const Box = styled.div`
-position:absolute; 
-${spacing}${palette}
-font-size:50px;
-font-weight:bold
-left:300;
-top:500;
-right:500;
-`;
+  position:absolute; 
+  ${spacing}${palette}
+  font-size:50px;
+  font-weight:bold
+  left:300;
+  top:500;
+  right:500;
+`
 
-//            <Img src={background}></Img>
 const Img = styled.img`
-width: 1000px;
-height: 500px;
-padding: 0px 10px 0px 10px;
-alt:background
+  width: 1000px;
+  height: 500px;
+  padding: 0px 10px 0px 10px;
+  alt:background
 `
 
   return (
     <div>      
       <div>
           <div>
-          <Img src={background}></Img>
+            <Img src={background}></Img>
             <Box p="1rem" color="grey">{props.content.email}</Box>      
           </div>
         </div>     
     </div>
   )
 }
+
 export default Main
