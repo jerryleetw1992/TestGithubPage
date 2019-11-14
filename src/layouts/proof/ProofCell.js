4c29c5e2211d17f9ff05fb46060032721e31b678
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2019-11-14 21:36:57 +0800

    Update

--- src/layouts/proof/ProofCell.js
+++ src/layouts/proof/ProofCell.js
@@ -11,7 +11,7 @@ import Paper from '@material-ui/core/Paper';
 const StyledTableCell = withStyles(theme => ({
     body: {
       fontSize: 14,
-      border: 200,
+      border: 0,
     },
   }))(TableCell);
   
