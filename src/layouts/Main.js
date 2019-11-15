eb9c50268bd6f5461a27492178e48289d4b90fab
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2019-11-15 15:45:09 +0800

    Update certificate view

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -3,15 +3,18 @@ import styled from 'styled-components';
 
 import Proof from './proof/Proof';
 import Certificate from './certificate/Certificate';
-import queryString from 'query-string'
+import queryString from 'query-string';
 
 
 const value= queryString.parse(window.location.search);
 const email=value.email;
 
 function Main(props) {
-    const Body = styled.div`
+  const ProofBody = styled.div`
     padding: 50px 0px 100px 0px;
+  `
+  const CertificateBody = styled.div`
+    padding: 0px 0px 0px 0px;
   `
 
   const [state, setState] = useState({});
@@ -19,7 +22,9 @@ function Main(props) {
   async function fetchMyAPI() {
     await fetch(`http://localhost:5000/api/view/users?displayName=&email=${email}&isIssuer=`)
       .then(res => res.json())
-      .then((returnData) => { setState(returnData.content[0]) })
+      .then((returnData) => { setState(returnData.content[0]) 
+      console.log(returnData);
+      })
       .catch(console.log)
   }
 
@@ -30,13 +35,15 @@ function Main(props) {
 
   if (Number(props.index) === 0) {
     return (
-      <Certificate content={state} />
+      <CertificateBody>
+        <Certificate content={state} />
+      </CertificateBody>
     )
   } else if (Number(props.index) === 1) {
     return (
-      <Body>
+      <ProofBody>
         <Proof />
-      </Body>
+      </ProofBody>
     )
   } else {
     return (
--- src/layouts/certificate/Certificate.js
+++ src/layouts/certificate/Certificate.js
@@ -1,43 +1,75 @@
 import React from 'react';
 import styled from 'styled-components';
-import background from '../../static/certificate.jpg';
-import { spacing, palette } from '@material-ui/system';
+import topLeft from '../../static/certificate/certificateTopLeft.svg';
+import bottomRight from '../../static/certificate/certificateBottomRight.svg';
+import topRight from '../../static/certificate/certificateTopRight.svg';
+import textCenter from '../../static/certificate/certificateRightCenter.svg';
 
 function Main(props){
 
-const Box = styled.div`
-  position:absolute; 
-  ${spacing}${palette}
-  font-size:50px;
-  font-weight:bold
-  left:300;
-  top:500;
-  right:500;
-`
-const Div = styled.div`
-border: 1px solid #000;
-background-image: url(${background});
-width: 2000px;
-height: 2000px;
+const Root = styled.div`
+  height: 100vh;
+  width: 100vw;
 `
 
-const Img = styled.img`
-  width: 1000px;
-  height: 500px;
-  padding: 0px 10px 0px 10px;
-  alt:background
+const Top = styled.div`
+  background: rgb(0, 0, 0);
+  height: 80%;
+  width: 100vw;
+`
+
+const TopLeft = styled.div`
+  position: absolute;
+  left: 0px;
+  width: 40%;
+`
+const TopRight = styled.div`
+  position: absolute;
+  right: 0px;
+  top: 10px;
+  width: 50%;
+`
+const Center = styled.div`
+  color: white;
+  text-align: center;
+`
+const Bottom = styled.div`
+  position: relative;
+  background: rgb(255, 255, 255);
+  height: 10vh;
+  width: 100vw;
+`
+
+const BottomRight = styled.img`
+  position: absolute;
+  height: 80%;
+  right: 10px;
+  bottom: 0px;
 `
 
   return (
-    <Div>      
-      123
-      {/* <div>
-          <div>
-            <Img src={background}></Img>
-            <Box p="1rem" color="grey">{props.content.email}</Box>      
-          </div>
-        </div>      */}
-    </Div>
+    <Root>
+      <Top>
+        <TopRight>
+          <img src={topRight}/>
+          <Center>
+            <h3>
+              {props.content.email}
+            </h3>
+            <h3>
+              {props.content.uid}
+            </h3>
+            <img src={textCenter}/>
+          </Center>
+        </TopRight>
+        <TopLeft>
+          <img src={topLeft}/>
+        </TopLeft>
+      </Top>
+      <Bottom>
+      <BottomRight src={bottomRight}/>
+      </Bottom>
+    </Root>    
   )
 }
 
