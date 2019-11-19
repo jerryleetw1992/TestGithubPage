544381c3e7839c2230976a79e69a051fd772e60a
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2019-11-20 04:13:20 +0800

    Fetch cert data and display

--- src/layouts/proof/Proof.js
+++ src/layouts/proof/Proof.js
@@ -12,26 +12,25 @@ import checkboxIcon from '../../static/check-square.svg';
 
 const value = queryString.parse(window.location.search);
 const email = value.email;
+const ipfs = value.ipfs;
 
 function createData(title, contnet) {
   return { title, contnet };
 }
-  
-const issuerRows = [
-  createData('Name:', 'ESCP Europe'),
-  createData('Legal references:', "ESCP Europe - Etablissement d'Enseignement Supérieur Consulaire (EESC) à but non lucratif au capital de 8.694.000 € - 824 644 587 RCS Paris - 3, rue Armand Moisant 75015 Paris, France"),
-  createData('Declaration of intent:', "We, ESCP Europe, intend to certify on a public blockchain our contributions through BCDiploma services."),
-  createData('Available at:', "https://certificate.bcdiploma.com/check/"),
-  createData('Blockchain Address:', "0xa4ed466ac0a65c68b0c48a0d2ed2c79efe271dd9"),
-];
 
-const ipfsRows = [
-  createData('Certified on:', '2019-06-13T11:13:18Z'),
-  createData('Certified data:', "N°2018618 Specialised Master (MS) Accredited by the Conférence des Grandes Écoles International Project Management This certificate is issued to Vincent LE GAL born on 1994-09-02 in Tarbes (65), France Awarded by the Board of Examiners of 2019-02-19 Paris, 2019-06-13 Gérard NAULLEAU Scientific Director ESCP Europe Frank BOURNOIS Executive President & Dean ESCP Europe A school of CCI PARIS ILE-DE-FRANCE"),
-  createData('Template:', "Mastère Spécialisé"),
-  createData('Template ID:', "7/0x01"),
-];
-  
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
 const Title = styled.div`
 	position: relative;
   width: 90%;
@@ -50,21 +49,37 @@ const Img = styled.img`
 
 function Main() {
 	const [holderRows, setHolderRows] = React.useState([]);
+	const [issuerRows, setIssuerRows] = React.useState([]);
+	const [ipfsRows, setIpfsRows] = React.useState([]);
 
-	async function fetchUserAPI() {
+	async function fetchUsersAPI() {
     await fetch(`http://localhost:5000/api/view/users?email=${email}`)
       .then(res => res.json())
       .then((returnData) => { 
 				const name = returnData.content[0].displayName
 				const email = returnData.content[0].email
         setHolderRows([...holderRows, createData('Name:', name), createData('Email:', email)]); 
-        console.log();
+      })
+      .catch(console.log)
+	}
+
+	async function fetchCertsAPI() {
+    await fetch(`http://localhost:5000/api/view/certs?ipfs=${ipfs}`)
+      .then(res => res.json())
+      .then((returnData) => {
+				setIssuerRows([...issuerRows,
+											createData('Time:', timeConverter(returnData.content.timestamp)),
+											createData('Issuer Hash:', returnData.content.issuer),
+											createData('Issuer Name:', returnData.content.issuerName),
+											createData('Type:', returnData.content.type)]); 
+				setIpfsRows([...ipfsRows, createData('IPFS:', returnData.content.ipfs)]);
       })
       .catch(console.log)
 	}
 	
 	useEffect(() => {
-    fetchUserAPI();
+		fetchUsersAPI();
+		fetchCertsAPI();
 	},[]);
 	
   return (
