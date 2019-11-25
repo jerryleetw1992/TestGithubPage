91adacc22b39b81b0b2d514f185336abc3c22e7e
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2019-11-25 19:25:35 +0800

    Update normal size

--- src/layouts/certificate/Certificate.js
+++ src/layouts/certificate/Certificate.js
@@ -7,14 +7,15 @@ import textCenter from '../../static/certificate/certificateRightCenter.svg';
 
 
 const Root = styled.div`
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   background: white;
 `
 const Top = styled.div`
-  background: rgb(0, 0, 0);
+  position: absolute;
   height: 80%;
-  width: 100vw;
+  width: 100%;
+  background: black;
 `
 const GridImage = styled.img`
   position: absolute;
@@ -30,27 +31,75 @@ const GridImage = styled.img`
 const TopRight = styled.div`
   position: absolute;
   right: 0px;
-  top: 10px;
-  width: 50%;
+  top: 0px;
+  width: 800px;
+  height: 100%;
+  @media (max-width: 1270px) {  
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    width: 600px;
+    height: 100%;
+  }
 `
-const Center = styled.div`
-  color: white;
+const TopRightTop = styled.div`
+  height: 20%;
+  background: black;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+const TopRightBottom = styled.div`
+  height: 80%;
   text-align: center;
+  background: black;
+`
+const TitleImage = styled.img`
+  height: 90%;
+  position: realive;
+  top: 50%;
+  left: 50%;
+`
+const Info = styled.div`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  color: white;
+  bottom: 200px;
+`
+const Text = styled.p`
+  font-size: 25px;
 `
 const Bottom = styled.div`
-  position: fixed;
-  background: red;
+  position: absolute;
+  background: white;
   height: 20%;
-  width: 100vw;
+  width: 100%;
   bottom: 0px;
   left: 0px;
 `
-const BottomRight = styled.img`
-  position: fixed;
+const Nav = styled.div`
+  position: absolute;
+  background: purple;
   height: 80px;
-  right: 10px;
-  bottom: 60px;
-  padding: 20px 10px;
+  width: 100%;
+  bottom: 0px;
+  left: 0px;
+`
+const LogoDiv = styled.div`
+  position: absolute;
+  background: white;
+  height: 100px;
+  width: 100%;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+const Logo = styled.img`
+  position: absolute;
+  width: 300px;
 `
 
 function Main(props){
@@ -58,21 +107,23 @@ function Main(props){
     <Root>
       <Top>
         <TopRight>
-          <img src={topRight} alt="Top Right"/>
-          <Center>
-            <h3>
-              {props.content.email}
-            </h3>
-            <h3>
-              {props.content.uid}
-            </h3>
-            <img src={textCenter}  alt="Center"/>
-          </Center>
+          <TopRightTop>
+            <TitleImage src={topRight} alt="Top Right"/>
+          </TopRightTop>
+          <TopRightBottom>
+            <Info>
+              <Text> {props.content.email} </Text>
+              <Text> {props.content.uid} </Text>
+              <img src={textCenter} alt="Center"/>
+            </Info>
+          </TopRightBottom>
         </TopRight>
         <GridImage src={topLeft} alt="Logo"/>
       </Top>
       <Bottom>
-        <BottomRight src={bottomRight}/>
+        <LogoDiv>
+          <Logo src={bottomRight} />
+        </LogoDiv>
       </Bottom>
     </Root>    
   )
